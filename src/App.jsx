5265668ce65deb7976aacd867c23026e0d525e40
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-05 17:45:11 +0200

    [UPD] Header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
-import './App.css'
+import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App() {
-  return <h1>ciao a tutti</h1>
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+
 }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+    return (
+        <header>
+            <figure>
+                <img src="dc-logo.png" alt="DC comics" />
+            </figure>
+            <ul>
+                <li>Characters</li>
+                <li className="highlight"><span className="blue-underline">Comics</span></li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
