b5bdb1b0ba7cf21184884e2f9338e3afe950e6b7
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-05 19:19:00 +0200

    [UPD] main completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,27 @@
 const Header = () => {
     return (
         <header>
-            <figure>
-                <img src="dc-logo.png" alt="DC comics" />
-            </figure>
-            <ul>
-                <li>Characters</li>
-                <li className="highlight"><span className="blue-underline">Comics</span></li>
-                <li>Movies</li>
-                <li>TV</li>
-                <li>Games</li>
-                <li>Collectibles</li>
-                <li>Videos</li>
-                <li>Fans</li>
-                <li>News</li>
-                <li>Shop</li>
-            </ul>
+            <div>
+                <figure>
+                    <img src="dc-logo.png" alt="DC comics" />
+                </figure>
+            </div>
+            <div>
+                <ul className="lista-header">
+                    <li>Characters</li>
+                    <li className="highlight"><span className="blue-underline">Comics</span></li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Collectibles</li>
+                    <li>Videos</li>
+                    <li>Fans</li>
+                    <li>News</li>
+                    <li>Shop</li>
+                </ul>
+            </div>
         </header>
-    )
-}
+    );
+};
 
 export default Header
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
 const Main = () => {
     return (
-        <div>
-
-        </div>
-    )
-}
+        <main>
+            <div className="main-top "><h1 className="text">--&gt; Content goes here &lt;--</h1></div>
+            <div className="main-bottom">
+                <ul className="Main-list">
+                    <li>
+                        <img className="img-size" src="src/assets/img/buy-comics-digital-comics.png" alt="digtal comics" />
+                        <span>Digital comics</span>
+                    </li>
+                    <li>
+                        <img className="img-size" src="src/assets/img/buy-comics-merchandise.png" alt="Merchandise" />
+                        <span>DC Merchandise</span>
+                    </li>
+                    <li>
+                        <img className="img-size" src="src/assets/img/buy-comics-subscriptions.png" alt="subricption" />
+                        <span>Subscription</span>
+                    </li>
+                    <li>
+                        <img className="locator-size" src="src/assets/img/buy-comics-shop-locator.png" alt="Shop locator" />
+                        <span>Shop locator</span>
+                    </li>
+                    <li>
+                        <img className="img-size" src="src/assets/img/buy-dc-power-visa.svg" alt="power visa" />
+                        <span>DC power visa</span>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    );
+};
 
 export default Main
