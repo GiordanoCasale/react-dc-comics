bf497ea11c51a64549a94e8021e385090ce93106
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-06 16:27:57 +0200

    [UPD] Refactor ComicCard component props; update thumb image source and adjust styles for ListComics layout

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,10 +1,10 @@
-const ComicCard = (props) => {
-    return (
+const ComicCard = ({ props }) => {
+    return (<div className="container-list card-container">
         <div className="prod-card">
-            <img src={props.src} alt="" />
+            <img src={props.thumb} alt="" />
             <h3>{props.title}</h3>
         </div>
-    )
+    </div>)
 }
 
 export default ComicCard
