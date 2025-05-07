6c09ccaee719079c03ab8d254d8f806a0534b128
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-07 14:50:36 +0200

    [UPD] Bonus completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,16 +8,13 @@ const Header = () => {
             </div>
             <div>
                 <ul className="lista-header">
-                    <li>Characters</li>
-                    <li className="highlight"><span className="blue-underline">Comics</span></li>
-                    <li>Movies</li>
-                    <li>TV</li>
-                    <li>Games</li>
-                    <li>Collectibles</li>
-                    <li>Videos</li>
-                    <li>Fans</li>
-                    <li>News</li>
-                    <li>Shop</li>
+                    {links.map((link) => (
+                        <li key={`link-${link.id}`}>
+                            <a href={link.href}>
+                                {link.label}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </header>
@@ -25,3 +22,66 @@ const Header = () => {
 };
 
 export default Header
+
+const links = [
+    {
+        id: 1,
+        href: "#",
+        label: "Characters",
+        current: false,
+    },
+    {
+        id: 2,
+        href: "#",
+        label: "Comics",
+        current: true,
+    },
+    {
+        id: 3,
+        href: "#",
+        label: "Movies",
+        current: false,
+    },
+    {
+        id: 4,
+        href: "#",
+        label: "TV",
+        current: false,
+    },
+    {
+        id: 5,
+        href: "#",
+        label: "Games",
+        current: false,
+    },
+    {
+        id: 6,
+        href: "#",
+        label: "Collectibles",
+        current: false,
+    },
+    {
+        id: 7,
+        href: "#",
+        label: "Videos",
+        current: false,
+    },
+    {
+        id: 8,
+        href: "#",
+        label: "Fans",
+        current: false,
+    },
+    {
+        id: 9,
+        href: "#",
+        label: "News",
+        current: false,
+    },
+    {
+        id: 10,
+        href: "#",
+        label: "Shop",
+        current: false,
+    }
+];
